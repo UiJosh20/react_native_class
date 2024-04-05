e238332fc210f0ca01c8cfd9f0e6e5ba62a10a8a
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-04-05 12:56:18 +0100

    first react native application

--- App.jsx
+++ App.jsx
@@ -5,17 +5,22 @@ import {
   ImageBackground,
   ScrollView,
   Button,
+  StatusBar
 } from "react-native";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
+import { useState } from "react";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [statusBar, setStatusBar] = useState(false)
   return (
     <View style={{ flex: 1, backgroundColor: "gray" }}>
+      <StatusBar backgroundColor="lightgreen" barStyle="dark-content" hidden={statusBar}/> 
       <ScrollView>
         <Navbar />
-        <Home/>
+        <Home setStatusBar={statusBar}/>
+        <Button  title="hide status bar" onPress={()=> setStatusBar(true)} /> 
         
       </ScrollView>
     </View>
--- Components/Home.jsx
+++ Components/Home.jsx
@@ -1,42 +1,63 @@
-
-import { useState } from 'react'
-import { Button, Image, Text, View, Pressable, Modal } from 'react-native'
-const heroimg = require('../assets/adaptive-icon.png')
+import { useState } from "react";
+import { Button, Image, Text, View, Pressable, Modal } from "react-native";
+const heroimg = require("../assets/adaptive-icon.png");
 
 const Home = () => {
-    const [IsModalVisible, setIsModalVisible] = useState(false)
-    const pressImg = () => {
-        console.log("Image pressed")
-    }
-    const pressText = () => {
-        console.log("text pressed")
-    }
+  const [IsModalVisible, setIsModalVisible] = useState(false);
+  
+  const pressImg = () => {
+    console.log("Image pressed");
+  };
+  const pressText = () => {
+    console.log("text pressed");
+  };
   return (
     <View>
-        <Pressable onPress={pressImg}>
-        <Image source={heroimg} style={{width:370, height: 300}}/>
-        </Pressable>
-        <Pressable onPress={pressText}>
-        <Text style={{fontSize: 13, paddingHorizontal: 20, textAlign: 'justify',}}>
+      <Pressable onPress={pressImg}>
+        <Image source={heroimg} style={{ width: 370, height: 300 }} />
+      </Pressable>
+      <Pressable onPress={pressText}>
+        <Text
+          style={{ fontSize: 13, paddingHorizontal: 20, textAlign: "justify" }}
+        >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat
         </Text>
-        </Pressable>
-        <View style={{paddingHorizontal: 20, marginTop: 20,}}>
-        <Button title="Press" onPress={() => setIsModalVisible(true)} />
+      </Pressable>
+      <View style={{ paddingHorizontal: 20, marginTop: 20 }}>
+        <Button title="Open modal" onPress={() => setIsModalVisible(true)} />
+        
+      </View>
+      <Modal
+        visible={IsModalVisible}
+        onRequestClose={() => setIsModalVisible(false)}
+        animationType="slide"
+        presentationStyle="pageSheet"
+      >
+        <View
+          style={{
+            backgroundColor: "lightblue",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            padding: 20,
+            borderRadius: 10,
+            shadowColor: "#000",
+          }}
+        >
+          <Text>Modal Content</Text>
+          <Button
+            title="Close"
+            color="midnightblue"
+            onPress={() => setIsModalVisible(false)}
+          />
         </View>
-        <Modal visible={IsModalVisible}>
-            <View style={{backgroundColor:"lightblue", flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20, borderRadius: 10, shadowColor: "#000",}}>
-                <Text>Modal Content</Text>
-                <Button title='Close' color="midnightblue" onPress={() => setIsModalVisible(false)}/>
-            </View>
-        </Modal>
-
+      </Modal>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
